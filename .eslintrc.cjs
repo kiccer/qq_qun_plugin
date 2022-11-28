d134a0b987efd6a79e37c566423e5436424bec20
kiccer <1072907338@qq.com>
2022-11-28 21:22:34 +0800

    🐎 ci: eslint 调整

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,15 @@ module.exports = {
     root: true,
     extends: [
         'plugin:vue/vue3-essential',
+        'plugin:vue/vue3-strongly-recommended',
+        'plugin:vue/vue3-recommended',
         '@vue/eslint-config-standard'
     ],
     parserOptions: {
         ecmaVersion: 'latest'
     },
     rules: {
-        indent: ['error', 4]
+        indent: ['error', 4],
+        'vue/html-indent': ['error', 4]
     }
 }
