544f4ae9ffc9ab6f0f113683466f2f3190da5b51
kiccer <1072907338@qq.com>
2022-11-29 22:29:27 +0800

    ✨ feat: 设置多入口

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,22 @@
 /* eslint-env node */
 module.exports = {
     root: true,
+
     extends: [
         'plugin:vue/vue3-essential',
         'plugin:vue/vue3-strongly-recommended',
         'plugin:vue/vue3-recommended',
         '@vue/eslint-config-standard'
     ],
+
     parserOptions: {
         ecmaVersion: 'latest'
     },
+
+    globals: {
+        chrome: 'readonly'
+    },
+
     rules: {
         indent: ['error', 4],
         'vue/html-indent': ['error', 4]
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,43 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue(), vueJsx()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command, mode, ssrBuild }) => {
+    return {
+        publicDir: './public',
+
+        build: {
+            rollupOptions: {
+                input: {
+                    background: './src/background.js',
+                    content: './src/content.js',
+                    popup: './src/popup.js',
+                    options: './src/options.js'
+                },
+                output: {
+                    dir: './extension-dist',
+                    entryFileNames: '[name].js'
+                }
+            }
+        },
+
+        plugins: [
+            vue(),
+            vueJsx()
+        ],
+
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+                '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+                '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+                '@router': fileURLToPath(new URL('./src/router', import.meta.url)),
+                '@store': fileURLToPath(new URL('./src/store', import.meta.url)),
+                '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+                '@utils': fileURLToPath(new URL('./src/utils', import.meta.url))
+            }
         }
     }
 })
